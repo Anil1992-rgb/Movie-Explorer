878dd6704d4449fe70206716cbca573ec70e6d04
Sammye Jones <sajones@ucdavis.edu>
2019-12-10 19:49:17 -0800

    backend started

---
+++ script.js
@@ -0,0 +1,25 @@
+$("#searchBtn").click(function () {
+    event.preventDefault();
+
+    $(".tmdbTitle").css("display", "block");
+    $(".otherTitle").css("display", "block");
+    
+
+    var query = $("#searchTerms").val();
+    console.log(query);
+    //had to prepend herokuapp to get rid of error message
+    var queryURL = "https://api.themoviedb.org/3/search/movie?api_key=0c2a1555ddc16a86620e39e37e74dfbb&language=en-US&query=" + query + "&page=1&include_adult=false";
+
+    $.ajax({
+        dataType: "json",
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        
+        //$("#tmdbTitle").html(reponse.results[0].title);
+        $(".tmdbTitle").html(response.results[0].title);
+    });
+});
+
+//https://image.tmdb.org/t/p/w500
