ea1053c4657986728a7716b6d0919e4ef55187bc
Sammye Jones <sajones@ucdavis.edu>
2019-12-11 22:45:18 -0800

    added some js, will finish in the AM

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-$("#searchBtn").click(function() {
-    event.preventDefault();
+
+function movieSearch() {
 
     $(".tmdbTitle").css("display", "block");
     $(".otherTitle").css("display", "block");
@@ -7,7 +7,7 @@ $("#searchBtn").click(function() {
 
     var query = $("#searchTerms").val();
     console.log(query);
-    //had to prepend herokuapp to get rid of error message
+    
     var queryURL = "https://api.themoviedb.org/3/search/movie?api_key=0c2a1555ddc16a86620e39e37e74dfbb&language=en-US&query=" + query + "&page=1&include_adult=false";
 
     $.ajax({
@@ -23,14 +23,23 @@ $("#searchBtn").click(function() {
         $(".tmdbTitle").html("Rating: " + response.results[0].vote_average + "<br>" + "<img src='" + "https://image.tmdb.org/t/p/w400" + tImg + "'>" + "<br>");
         $(".otherTitle").html("!PLACEHOLDER!");
     });
+
+    $("#searchTerms").val("");
+  };
+
+  $("#searchTerms").keypress(function(event) {
+    var key = event.which;
+    if (key == 13) {
+        movieSearch();
+    }
+});
+
+$("#searchBtn").click(function() {
+    event.preventDefault();
+
+    movieSearch();
 });
 
 
 
 
-// $("#searchTerms").keypress(function(event) {
-//     var key = event.which;
-//     if (key == 13) {
-//         callWeatherApi();
-//     }
-// });
